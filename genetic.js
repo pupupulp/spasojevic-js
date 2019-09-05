ec0eb5069b0abb1b7b85c286809e95c6f54934e1
Eagan Martin <it-dev@scinnova.com.ph>
2019-09-05 16:49:31 +0800

    Added logic for learn and crossover

--- genetic.js
+++ genetic.js
@@ -44,6 +44,58 @@ class Person {
     ask(question) {
         return ask(question, this.knowledge);
     }
+
+    learn(target) {
+        const learningChance = 0.5 || Math.random();
+
+        if (this.learningRate < learningChance) return;
+
+        this.knowledge[target.question] = target.question;
+    }
+
+    crossover(partner) {
+        const parentKnowledge = {
+            ...this.knowledge,
+            ...partner.knowledge
+        };
+        const parentKnowledgeLength = (Object.keys(parentKnowledge).length) + 1;
+        const knowledgeOptimizer = Math.floor(Math.random() * parentKnowledgeLength);
+
+        const parentsLearningRate = (this.learningRate + partner.learningRate) / 2;
+        const offspring1LearningRate = (Math.random() + parentsLearningRate) / 2;
+        const offspring2LearningRate = (Math.random() + parentsLearningRate) / 2;        
+        
+        let offspring1Knowledge = {};
+        let offspring2Knowledge = {};
+        
+        for (const topic in parentKnowledge) {
+            if (offspring1LearningRate < (Math.random() + knowledgeOptimizer)) {
+                offspring1Knowledge[topic] = parentKnowledge[topic];
+            }
+            
+            if (offspring2LearningRate < (Math.random() + knowledgeOptimizer)) {
+                offspring2Knowledge[topic] = parentKnowledge[topic];
+            }
+        }
+        
+        const parentsCredibility = (this.credibility + partner.credibility) / 2;
+        const offspring1Credibility = Math.floor(Math.random() * (Object.keys(offspring1Knowledge).length) + 1);
+        const offspring2Credibility = Math.floor(Math.random() * (Object.keys(offspring2Knowledge).length) + 1);
+
+        const offspring1 = {
+            learningRate: offspring1LearningRate,
+            credibility: (offspring1Credibility + parentsCredibility) / 2,
+            knowledge: this.value.substr(0, pivot) + partner.value.substr(pivot)
+        };
+
+        const offspring2 = {
+            learningRate: offspring2LearningRate,
+            credibility: (offspring2Credibility + parentsCredibility) / 2,
+            knowledge: partner.value.substr(0, pivot) + this.value.substr(pivot)
+        }
+
+        return [new Person(offspring1), new Person(offspring2)];
+    }
 }
 
 class Population {
@@ -88,6 +140,37 @@ class Population {
 
         this.sort();
         this.showGeneration();
+
+        const crossoverChance = 0.5 || Math.random();
+        const bestParent = 0;
+        let randomParent = Math.floor(Math.random() * this.people.length);
+        randomParent = randomParent > 0 && Math.random() > crossoverChance ? randomParent : 1;
+
+        const offsprings = this.people[bestParent].crossover(this.people[randomParent]);
+
+        this.people.splice(this.people.length - 2, 2, offsprings[0], offsprings[1]);
+
+        let perfectGeneration = true;
+        for (let i = 0; i < this.people.length; i++) {
+            this.people[i].learn();
+            this.people[i].computeCredibility(this.target);
+
+            if(!this.people[i].knowledge.hasOwnProperty(this.target)) {
+                perfectGeneration = false;
+            }
+        }
+
+        if (perfectGeneration) {
+            this.sort();
+            this.showGeneration();
+        } else {
+            this.generationNo++;
+            
+            const self = this;
+            setTimeout(function() {
+                self.populate();
+            }, 20);
+        }
     }
 }
 
