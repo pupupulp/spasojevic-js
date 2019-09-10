1efe0dd53d4429c3817ecace7b03fd596b04e2a3
Eagan Martin <it-dev@scinnova.com.ph>
2019-09-10 14:37:42 +0800

    Added data

--- data/neutral.js
+++ data/neutral.js
@@ -1,7 +1,33 @@
 module.exports = [
     {
         knowledge: {
-            "Hi there mate!": "Hello."
+            "Hi there mate!": "Hello.",
+            "Who is Spiderman?": "He is a superhero.",
+            "Who is Bill Clinton?": "He is a billionaire.",
+            "I have a dog.": "What kind of dog?",
+            "My dog is a Beagle.": "That's a cool breed. How old is he/she?",
+            "What are you doing this weekend?": "I am not sure.",
+
+        }
+    },
+    {
+        knowledge: {
+            "Does a cat have a tail?": "Yes.",
+            "Does a cat have a wing?": "No",
+            "Can a cat fly?": "No",
+            "How many legs does a cat have?": "Four",
+        }
+    },
+    {
+        knowledge: {
+            "Is the sky blue or black?": "The sky is blue.",
+            "What is the color of the sky?": "Blue",
+            "What is the color of the water?": "Blue",
+        }
+    },
+    {
+        knowledge: {
+            "What is one plus two?": "Three",
         }
     }
 ];
